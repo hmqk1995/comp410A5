29c05b2dcf75edcc200c6c82d1239d82bde73e14
Kun Qian <kun@unc.edu>
2018-04-17 19:33:36 -0400

    fail efficiency at 2000000

--- A5_Dijkstra/DiGraphPlayground.java
+++ A5_Dijkstra/DiGraphPlayground.java
@@ -1,4 +1,8 @@
 package A5_Dijkstra;
+import java.security.SecureRandom;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.Random;
 
 public class DiGraphPlayground {
 
@@ -13,6 +17,7 @@ public class DiGraphPlayground {
       //    -- etc.
       // in order to convince yourself your code is producing
       // the correct behavior
+      effiencyTest();
       exTest();
     }
   
@@ -33,4 +38,75 @@ public class DiGraphPlayground {
       System.out.println("numNodes: "+d.numNodes());
       d.print();
     }
+    
+    public static void effiencyTest() {
+    	  DiGraph d = new DiGraph();
+    	  RandomString r = new RandomString(10);
+    	  String sr = r.nextString();
+    	  d.addNode(1, sr);
+    	  String mr = "";
+    	  for (int i = 0; i < 2000000; i++) {
+    		  mr = r.nextString();
+    		  d.addNode(i+2, mr);
+    		  d.addEdge(i, sr, mr, 0, null);
+    		  sr = mr;
+    	  }
+//    	  d.print();
+    }
+}
+
+class RandomString {
+
+    /**
+     * Generate a random string.
+     */
+    public String nextString() {
+        for (int idx = 0; idx < buf.length; ++idx)
+            buf[idx] = symbols[random.nextInt(symbols.length)];
+        return new String(buf);
+    }
+
+    public static final String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public static final String lower = upper.toLowerCase(Locale.ROOT);
+
+    public static final String digits = "0123456789";
+
+    public static final String alphanum = upper + lower + digits;
+
+    private final Random random;
+
+    private final char[] symbols;
+
+    private final char[] buf;
+
+    public RandomString(int length, Random random, String symbols) {
+        if (length < 1) throw new IllegalArgumentException();
+        if (symbols.length() < 2) throw new IllegalArgumentException();
+        this.random = Objects.requireNonNull(random);
+        this.symbols = symbols.toCharArray();
+        this.buf = new char[length];
+    }
+
+    /**
+     * Create an alphanumeric string generator.
+     */
+    public RandomString(int length, Random random) {
+        this(length, random, alphanum);
+    }
+
+    /**
+     * Create an alphanumeric strings from a secure generator.
+     */
+    public RandomString(int length) {
+        this(length, new SecureRandom());
+    }
+
+    /**
+     * Create session identifiers.
+     */
+    public RandomString() {
+        this(21);
+    }
+
 }
